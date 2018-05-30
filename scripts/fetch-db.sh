1a57ded4e56e94e673e8f4bc1a3280c065095dc1
gahag <gabriel.s.b@live.com>
2018-05-30 12:59:07 -0300

    Updated and splitted fetch-db script. Created scripts folder. Updated database.
    Removed original directory: too big to version control.

---
+++ scripts/fetch-db.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+declare -A apis=(
+  ["uasgs"]="http://compras.dados.gov.br/licitacoes/v1/uasgs.json"
+  ["licitacoes"]="http://compras.dados.gov.br/licitacoes/v1/licitacoes.json"
+  ["fornecedores"]="http://compras.dados.gov.br/fornecedores/v1/fornecedores.json"
+  ["orgaos"]="http://compras.dados.gov.br/licitacoes/v1/orgaos.json"
+  ["contratos"]="http://compras.dados.gov.br/contratos/v1/contratos.json"
+  ["municipios"]="http://compras.dados.gov.br/fornecedores/v1/municipios.json"
+)
+
+declare -A limits=(
+  ["fornecedores"]="$(seq 0 500 10000)"
+  ["licitacoes"]="$(seq 0 500 1023999)"
+  ["orgaos"]="0 500"
+  ["uasgs"]="$(seq 0 500 28054)"
+  ["contratos"]="$(seq 0 500 10000)"
+  ["municipios"]="$(seq 0 500 5000)"
+)
+
+
+for api in "${!apis[@]}"; do
+  for i in ${limits[$api]}; do
+    curl "${apis[$api]}?offset=$i" | jq . > data/original/$api/$api-$i.json
+  done
+done
