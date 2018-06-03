2d9279b23306b9143266f44d2c706d8adffbb107
gahag <gabriel.s.b@live.com>
2018-06-03 20:13:26 -0300

    Added time info to do-the-thing script.

--- scripts/do-the-thing-zhu-li.sh
+++ scripts/do-the-thing-zhu-li.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-# echo Fetching:
-# scripts/fetch-json.sh
-# echo
-
-echo Processing.
-time scripts/process-jsons.sh
-echo
-
-echo Normalizing.
-time scripts/normalize-jsons.py
-echo
-
-echo Building sql.
-time scripts/build-sql.sh
-echo
-
-echo Dropping db.
-time scripts/drop-db.sh $1
-echo
-
-echo Init db.
-time scripts/init-db.sh $1
-echo
+time {
+  # echo Fetching:
+  # scripts/fetch-json.sh
+  # echo
+  
+  echo Processing.
+  time scripts/process-jsons.sh
+  echo
+  
+  echo Normalizing.
+  time scripts/normalize-jsons.py
+  echo
+  
+  echo Building sql.
+  time scripts/build-sql.sh
+  echo
+  
+  echo Dropping db.
+  time scripts/drop-db.sh $1
+  echo
+  
+  echo Init db.
+  time scripts/init-db.sh $1
+  echo
+}
